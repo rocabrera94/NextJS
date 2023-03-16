3c11715dbe861161455439e3b957c3d9bf1f236a
Rodrigo Cabrera <rocabrera1994@gmail.com>
2023-03-15 21:45:57 -0300

    agregar estilo a [id].js

--- users/pages/Id/[id].js
+++ users/pages/Id/[id].js
@@ -1,16 +1,21 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
+import idStyles from '../../styles/[id].module.css'
+
 const User = () => {
   const router = useRouter()
   const {id, userName, firstName, lastName} = router.query
   router
   return (
-    <div>
-      <p>Name:{firstName}</p>
-      <p>LastName:{lastName}</p>
-      <p>userName:{userName}</p>
-      <p>Id: #{id}</p>
-      <Link href='/' >go back home</Link>
+    <div className={idStyles.main}>
+      <div className={idStyles.container}>
+        <p className={idStyles.name}>Name:{firstName} {lastName}</p>
+        <div className={idStyles.data}>
+          <p className={idStyles.userName}>userName:{userName}</p>
+          <p className={idStyles.id}>Id: #{id}</p>
+        </div>
+        <Link href='/' className={idStyles.link}>go back home</Link>
+      </div>
     </div>
   )
 }
